8b937d10703ad853d63c79c73a77a9dea64739ae
Your Name <vanglongdv123@gmail.com>
2022-02-26 18:41:59 +0700

    main

--- src/main/Game.java
+++ src/main/Game.java
@@ -6,18 +6,90 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import week_04.shapes.ShapeT;
+
+
 public class Game {
 	private Board board;
 	private Player []player = new Player[2];
-	private Spot []spots = new Spot[32];
+	private Pawn []Pawns = new Pawn[32];
+	private List<Spot> bspots = new ArrayList<Spot>();
+	private List<Spot> wspots = new ArrayList<Spot>();
 	public Game() {
-		super();
+		Rook brook      = new Rook(0, 0, false, false, "rook");
+		bspots.add(brook);
+		Knight bkinght  = new Knight(1, 0, false, false, "knight");
+		bspots.add(bkinght);
+		Bishop bbishop  = new Bishop(2, 0, false, false, "bishop");
+		bspots.add(bbishop);
+		Queen bqueen    = new Queen(3, 0, false, false, "queen");
+		bspots.add(bqueen);
+		King bking      = new King(4, 0, false, false, "king");
+		bspots.add(bking);
+		Bishop bbishop2 = new Bishop(5, 0, false, false, "bishop");
+		bspots.add(bbishop2);
+		Knight bknight2  = new Knight(6, 0, false, false, "knight");
+		bspots.add(bknight2);
+		Rook brook2     = new Rook(7, 0, false, false, "rook");
+		bspots.add(brook2);
+		Pawn bpawn1     = new Pawn(1, 1, false, false, "pawn");
+		bspots.add(bpawn1);
+		Pawn bpawn2     = new Pawn(2, 1, false, false, "pawn");
+		bspots.add(bpawn2);
+		Pawn bpawn3     = new Pawn(3, 1, false, false, "pawn");
+		bspots.add(bpawn3);
+		Pawn bpawn4     = new Pawn(4, 1, false, false, "pawn");
+		bspots.add(bpawn4);
+		Pawn bpawn5     = new Pawn(5, 1, false, false, "pawn");
+		bspots.add(bpawn5);
+		Pawn bpawn6     = new Pawn(6, 1, false, false, "pawn");
+		bspots.add(bpawn6);
+		Pawn bpawn7     = new Pawn(7, 1, false, false, "pawn");
+		bspots.add(bpawn7);
+		Pawn bpawn8     = new Pawn(0, 1, false, false, "pawn");
+		bspots.add(bpawn8);
+
+		Rook wrook      = new Rook(0, 7, true, false, "rook");
+		wspots.add(wrook);
+		Pawn wkinght    = new Pawn(1, 7, true, false, "knight");
+		wspots.add(wknight);
+		Bishop wbishop  = new Bishop(2, 7, true, false, "bishop");
+		wspots.add(wbishop);
+		Queen wqueen    = new Queen(3, 7, true, false, "queen");
+		wspots.add(wqueen);
+		King wking      = new King(4, 7, true, false, "king");
+		wspots.add(wking);
+		Bishop wbishop2 = new Bishop(5, 7, true, false, "bishop");
+		wspots.add(wbishop2);
+		Knight wknight2  = new Knight(6, 7, true, false, "knight");
+		wspots.add(wknight2);
+		Rook wrook2     = new Rook(7, 7, true, false, "rook");
+		wspots.add(wrook2);
+		Pawn wpawn1     = new Pawn(1, 6, true, false, "pawn");
+		wspots.add(wpawn1);
+		Pawn wpawn2     = new Pawn(2, 6, true, false, "pawn");
+		wspots.add(wpawn2);
+		Pawn wpawn3     = new Pawn(3, 6, true, false, "pawn");
+		wspots.add(wpawn3);
+		Pawn wpawn4     = new Pawn(4, 6, true, false, "pawn");
+		wspots.add(wpawn4);
+		Pawn wpawn5     = new Pawn(5, 6, true, false, "pawn");
+		wspots.add(wpawn5);
+		Pawn wpawn6     = new Pawn(6, 6, true, false, "pawn");
+		wspots.add(wpawn6);
+		Pawn wpawn7     = new Pawn(7, 6, true, false, "pawn");
+		wspots.add(wpawn7);
+		Pawn wpawn8     = new Pawn(0, 6, true, false, "pawn");
+		wspots.add(wpawn8);
+		
 		this.board = new Board();
 		BufferedImage all = null;
 		try {
@@ -55,6 +127,9 @@ public class Game {
 				}
 			}
 		};
+		for(Spot spot : spots) {
+			
+		}
 		frame.add(pn);
 		frame.setDefaultCloseOperation(3);
 		frame.setVisible(true);
