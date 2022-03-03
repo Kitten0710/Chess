15004e543287d86c9e109c45a8585fbf117371e7
Your Name <vanglongdv123@gmail.com>
2022-03-03 17:35:04 +0700

    validate pawn

--- src/game/Pawn.java
+++ src/game/Pawn.java
@@ -27,8 +27,81 @@ public class Pawn implements Spot{
 
 	@Override
 	public void move(int x, int y) {
-		boolean canMove = true; 
-		
+		System.out.println(a.getX() + " " + a.getY());
+		boolean canMove = false; 
+		if(a.isColor() == false) {	// nếu là tốt đen
+			if(Math.abs(x - a.getX()) == 1 && y == a.getY() + 1) {	// kiểm tra giết
+				for(Spot p : Game.getWspots()) {
+					if(p.getPiece().getX() == x && p.getPiece().getY() == y) {
+						canMove = true;
+						a.move(x, y, canMove);
+						return;
+					}
+				}
+			} else if(a.getY() == 1) {	// nếu ở vị trí xuất phát
+				if(y - a.getY() <= 2 && x == a.getX()) {	// có thể đi 2 bước
+					canMove = true;
+					for(Spot p : Game.getWspots()) {
+						if(p.getPiece().getX() == x && p.getPiece().getY() == y) {
+							canMove = false;
+							a.move(x, y, canMove);
+							return;
+						}
+					}
+					a.move(x, y, canMove);
+					return;
+				} 
+			} else {	// các bước sau
+				if(y - a.getY() == 1 && x == a.getX()) {	// có thể đi 2 bước
+					canMove = true;
+					for(Spot p : Game.getWspots()) {
+						if(p.getPiece().getX() == x && p.getPiece().getY() == y) {
+							canMove = false;
+							a.move(x, y, canMove);
+							return;
+						}
+					}
+					a.move(x, y, canMove);
+					return;
+				} 
+			}
+		} else {	// nếu là tốt trắng
+			if(Math.abs(x - a.getX()) == 1 && a.getY() == y + 1) {	// kiểm tra giết
+				for(Spot p : Game.getBspots()) {
+					if(p.getPiece().getX() == x && p.getPiece().getY() == y) {
+						canMove = true;
+						a.move(x, y, canMove);
+						return;
+					}
+				}
+			} else if(a.getY() == 6) {	// nếu ở vị trí xuất phát
+				if(a.getY() - y <= 2 && x == a.getX()) {	// có thể đi 2 bước
+					canMove = true;
+					for(Spot p : Game.getBspots()) {
+						if(p.getPiece().getX() == x && p.getPiece().getY() == y) {
+							canMove = false;
+							a.move(x, y, canMove);
+							return;
+						}
+					}
+					a.move(x, y, canMove);
+					return;
+				} 
+			} else {	// các bước sau
+				if(a.getY() - y == 1 && x == a.getX()) {	// có thể đi 2 bước
+					canMove = true;
+					for(Spot p : Game.getBspots()) {
+						if(p.getPiece().getX() == x && p.getPiece().getY() == y) {
+							canMove = false;
+							a.move(x, y, canMove);
+							return;
+						}
+					}
+					a.move(x, y, canMove);
+					return;
+				} 
+			}
+		}
 		a.move(x, y, canMove);
 	}
 
