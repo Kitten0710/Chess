a06a36f3c7111e79985c9fd0b9e3a9d9c79045d3
Admin <Admin@LAPTOP-SEAFD920>
2022-03-04 21:34:39 +0700

    moi set time thoi chua gan dc vao giao dien

--- src/time/set_time.java
+++ src/time/set_time.java
@@ -1,5 +1,26 @@
 package time;
-
+import java.util.Timer;
+import java.util.TimerTask;
 public class set_time {
-
+	static int interval;
+	static Timer time;
+	private static final int setInterval() {
+	    if (interval == 1)
+	        time.cancel();
+	    return --interval;
+	}
+	public static void main(String[] args) {
+		String seconds = "120";
+		int delay = 1000;
+	    int period = 1000;
+	    time = new Timer();
+	    interval = Integer.parseInt(seconds);
+	    System.out.println(seconds);
+	    time.scheduleAtFixedRate(new TimerTask() {
+	    	public void run() {
+	            System.out.println(setInterval());
+	        }
+	    }, delay, period);
+	}
 }
+
