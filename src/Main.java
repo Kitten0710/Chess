35b59f054752b9435da77316b125c45251fd6ce1
Admin <Admin@LAPTOP-SEAFD920>
2022-03-06 16:30:51 +0700

    time set

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import model.Game;
+import time.set_time;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.play();
+		set_time stop = new set_time();
 	}
 
 }
