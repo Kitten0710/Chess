40bf689f6a0b8f44dd5317bd4bd5cf8af8ee65b3
Your Name <vanglongdv123@gmail.com>
2022-03-18 19:30:38 +0700

    move

--- src/model/spot/piece/Queen.java
+++ src/model/spot/piece/Queen.java
@@ -165,7 +165,7 @@ public class Queen implements Spot{
 			}
 		}
 		if(canMove == true) {
-			if(Game.getSpot(x*64, y*64) != null && Game.getSpot(x*64, y*64).getPiece().isColor() == a.isColor()) {
+			if(Game.getSpot(x*81, y*81) != null && Game.getSpot(x*81, y*81).getPiece().isColor() == a.isColor()) {
 				canMove = false;
 			}
 		}
