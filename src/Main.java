9dfe797e06c33a66061f9825e5c0199af1e7a132
Admin <Admin@LAPTOP-SEAFD920>
2022-03-24 15:39:00 +0700

    d

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Controller controller = new Controller();
-//		set_time_white white = new set_time_white();
-//		set_time_black black = new set_time_black();
+
 	}
 
 }
